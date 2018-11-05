5878b860c5e94a09e136cae1507fa77aaa8d9be8
Daniel Schruhl <danielschruhl@gmail.com>
2018-11-05 20:49:49 +0100

    change wording

--- src/test/scala/com/github/meandor/MobiusChairTest.scala
+++ src/test/scala/com/github/meandor/MobiusChairTest.scala
@@ -8,12 +8,12 @@ class MobiusChairTest extends FeatureSpec with Matchers {
   val basePath = "src/test/resources/filesystem"
   val localHDFS: LocalFileSystem = FileSystem.getLocal(new Configuration())
 
-  feature("get latest generation in path") {
-    scenario("nothing existing yet") {
+  feature("should get latest generation in path") {
+    scenario("should return 0000 when nothing existing yet") {
       MobiusChair.latestGeneration(localHDFS, s"$basePath/inFoo/0001") shouldBe "0000"
     }
 
-    scenario("n generations existing") {
+    scenario("should return latest when n generations existing") {
       MobiusChair.latestGeneration(localHDFS, s"$basePath/inFooBar/0002") shouldBe "0009"
     }
   }
